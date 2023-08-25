447c5820f344ae2322d24e34fb40171698c336d8
snack <kwansiknam@gmail.com>
2023-08-25 20:55:55 +0900

    Feat: "Add jump constraints"

--- wallarun/GameScene.swift
+++ wallarun/GameScene.swift
@@ -7,47 +7,64 @@
 
 import SpriteKit
 
-class GameScene: SKScene {
+class GameScene: SKScene, SKPhysicsContactDelegate {
     
     private var wallaby: SKShapeNode!
     private var ground: SKSpriteNode!
+    private var isJumping: Bool = false
     
     override func didMove(to view: SKView) {
         self.backgroundColor = .gray
+        self.physicsWorld.gravity = CGVector(dx: 0, dy: -20)
+        self.physicsWorld.contactDelegate = self
         createWallaby(for: self.size)
         createGround(for: self.size)
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        wallaby.physicsBody?.applyImpulse(CGVector(dx: 0, dy: 40))
+        if !isJumping {
+            isJumping = true
+            wallaby.physicsBody?.applyImpulse(CGVector(dx: 0, dy: 500))
+        }
     }
     
     func createWallaby(for size: CGSize) {
-        wallaby = SKShapeNode(circleOfRadius: 20)
+        wallaby = SKShapeNode(circleOfRadius: 60)
         
         wallaby.fillColor = .blue
-        wallaby.position = CGPoint(x: self.size.width/2, y: self.size.height/2)
+        wallaby.position = CGPoint(x: 150, y: self.size.height/2)
         
-        self.physicsWorld.gravity = CGVector(dx: 0, dy: -15)
-        
-        wallaby.physicsBody = SKPhysicsBody(circleOfRadius: 20)
+        wallaby.physicsBody = SKPhysicsBody(circleOfRadius: 60)
         wallaby.physicsBody?.isDynamic = true
         wallaby.physicsBody?.allowsRotation = false
+        
+        wallaby.physicsBody?.categoryBitMask = 1
+        wallaby.physicsBody?.contactTestBitMask = 2
+        wallaby.physicsBody?.collisionBitMask = 2
         
         self.addChild(wallaby)
     }
     
     func createGround(for size: CGSize) {
-        ground = SKSpriteNode(color: .brown, size: CGSize(width: self.size.width, height: 100))
+        ground = SKSpriteNode(color: .brown, size: CGSize(width: self.size.width, height: 60))
         
-        ground.position = CGPoint(x: self.size.width/2, y: 50)
+        ground.position = CGPoint(x: self.size.width/2, y: 20)
         ground.physicsBody = SKPhysicsBody(rectangleOf: ground.size)
         ground.physicsBody?.isDynamic = false
+        
+        ground.physicsBody?.categoryBitMask = 2
+        ground.physicsBody?.contactTestBitMask = 1
+        ground.physicsBody?.collisionBitMask = 1
         
         self.addChild(ground)
     }
     
-    func didBegin(contact: SKPhysicsContact) {
+    func didBegin(_ contact: SKPhysicsContact) {
+        let bodyA = contact.bodyA
+        let bodyB = contact.bodyB
         
+        if bodyA.node == wallaby || bodyB.node == wallaby {
+            isJumping = false
+        }
     }
 }
