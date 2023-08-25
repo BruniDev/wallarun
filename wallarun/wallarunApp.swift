4693e1a7a85873eb1d9bb429fe406d482e560c5c
hyeondxx@gmail.com <hyeondxx@gmail.com>
2023-08-25 18:18:45 +0900

    Initial Commit

---
+++ wallarun/wallarunApp.swift
@@ -0,0 +1,17 @@
+//
+//  wallarunApp.swift
+//  wallarun
+//
+//  Created by 정현 on 2023/08/25.
+//
+
+import SwiftUI
+
+@main
+struct wallarunApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
