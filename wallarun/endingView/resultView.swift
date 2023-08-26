76e6c8fa9387ad18ee697da989f6d1993c9c26e8
hyeondxx@gmail.com <hyeondxx@gmail.com>
2023-08-26 19:37:51 +0900

    Feat: "EndView Added"
    ResultView not added -> after collision happen

---
+++ wallarun/endingView/resultView.swift
@@ -0,0 +1,20 @@
+//
+//  resultView.swift
+//  wallarun
+//
+//  Created by 정현 on 2023/08/26.
+//
+
+import SwiftUI
+
+struct resultView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/) //여기는 충돌 여부에 따라서 나오는 팝업창입니다.
+    }
+}
+
+struct resultView_Previews: PreviewProvider {
+    static var previews: some View {
+        resultView()
+    }
+}
